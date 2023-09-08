36e7868c5e6df458efe5eec3861a953508696c58
AndrezRecalde <azw1021@gmail.com>
2023-09-08 14:53:54 -0500

    Se agrega estructura IF y Switch

--- main.go
+++ main.go
@@ -2,12 +2,29 @@ package main
 
 import (
 	"fmt"
-	"godesdecero/variables"
+	/* "godesdecero/variables" */
+	"runtime"
 )
 
 func main() {
 	//variables.RestoVariables()
-	estado, texto := variables.ConvierteaTexto(1588)
+	/* estado, texto := variables.ConvierteaTexto(1588)
 	fmt.Println(estado)
-	fmt.Println(texto)
+	fmt.Println(texto) */
+
+	os := runtime.GOOS
+	if os == "Linux." || os == "OS X." {
+		fmt.Println("No estas wn Windows")
+	} else {
+		fmt.Println("Estas en Windows: ", os)
+	}
+
+	switch os {
+	case "linux":
+		fmt.Println("Esto es Linux")
+	case "windows":
+		fmt.Println("Esto es Windows")
+	default:
+		fmt.Println("Este SO es: ", os)
+	}
 }
